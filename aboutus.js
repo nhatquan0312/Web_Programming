02d934c7a118efae7cf4885f19b27763896b875f
nhatquan0312 <57495143+nhatquan0312@users.noreply.github.com>
2021-04-30 11:21:16 +0700

    update new function

---
+++ aboutus.js
@@ -0,0 +1,45 @@
+//First Profile
+var modalBtn= document.querySelector('.modal-btn');
+var modalBg= document.querySelector('.modal-bg');
+var modalCloseBtn=document.querySelector('.modal-close-btn');
+
+modalBtn.addEventListener('click',function() {
+    modalBg.classList.add('bg-active');
+  });
+  modalCloseBtn.addEventListener('click',function(){
+     modalBg.classList.remove('bg-active');
+  });
+//Second Profile
+var modalBtn2= document.querySelector('.modal-btn-2');
+var modalBg2= document.querySelector('.modal-bg-2');
+var modalCloseBtn2=document.querySelector('.modal-close-btn-2');
+
+modalBtn2.addEventListener('click',function() {
+    modalBg2.classList.add('bg-active');
+  });
+  modalCloseBtn2.addEventListener('click',function(){
+     modalBg2.classList.remove('bg-active');
+  });
+//Third Profile
+var modalBtn3= document.querySelector('.modal-btn-3');
+var modalBg3= document.querySelector('.modal-bg-3');
+var modalCloseBtn3=document.querySelector('.modal-close-btn-3');
+
+modalBtn3.addEventListener('click',function() {
+    modalBg3.classList.add('bg-active');
+  });
+  modalCloseBtn3.addEventListener('click',function(){
+     modalBg3.classList.remove('bg-active');
+  });
+//Fourth Profile
+var modalBtn4= document.querySelector('.modal-btn-4');
+var modalBg4= document.querySelector('.modal-bg-4');
+var modalCloseBtn4=document.querySelector('.modal-close-btn-4');
+
+modalBtn4.addEventListener('click',function() {
+    modalBg4.classList.add('bg-active');
+  });
+  modalCloseBtn4.addEventListener('click',function(){
+     modalBg4.classList.remove('bg-active');
+  });
+
