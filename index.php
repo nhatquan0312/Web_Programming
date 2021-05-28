936b8f8669c08a48b6682f63eb6e3243f55b630c
s3879069 <s3879069@rmit.edu.vn>
2021-05-28 14:31:16 +0700

    finished php2

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+  session_start();
+
+  require 'functions.php';
+
+      //products
+
+      $products = read_all_products();
+
+      $featured_products=[];
+
+      $count = 0;
+
+      echo '<ul>';
+      foreach ($products as $p) {
+          if ($p['featured_in_mall'] === "TRUE") {
+            $featured_products[] = $p;
+            $count++;
+            if ($count == 10) {
+                break;
+            }
+          }
+      }
+
+      //stores
+      $stores = read_all_stores();
+
+      $featured_stores=[];
+
+      $count_for_stores = 0;
+
+      echo '<ul>';
+      foreach ($stores as $s) {
+          if ($s['featured'] === "TRUE") {
+            $featured_stores[] = $s;
+            $count_for_stores++;
+            if ($count_for_stores == 10) {
+                break;
+            }
+          }
+      }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -65,19 +109,37 @@
 
     <div class="title"><strong>NEW STORES</strong><span><a href="#">MORE</a></span></div>
 
+    <?php
+      $sorted_by_date_stores = sort_by_group($stores, 'created_time', false);
+
+    ?>
+
 
     <div class="wrapper-store">
       <div class="prev-btn"><i class="fas fa-chevron-left" id="button"></i></div>
       <div class="stores-container">
+        <?php
+          for($i=0; $i < 10; $i++) {
+        ?>
+
+
+
         <div class="store-image">
           <a href='mainpage/store/store.html' style='text-decoration: none;'><img id="str-img" src='img/Uniqlo-logo(1).jpg'></a>
+          <h3> <?=$sorted_by_date_stores[$i]['name']?> </h3>
         </div>
-        <div class="store-image">
+
+        <?php 
+          }
+        ?>
+
+
+        <!--div class="store-image">
           <a href='mainpage/store/store2.html' style='text-decoration: none;'><img id="str-img" src='img/MLB-OG(1).png'></a>
         </div>
         <div class="store-image">
           <a href='mainpage/store/store3.html' style='text-decoration: none;'><img id="str-img" src='img/990__1511456189_555_McDonalds.png'></a>
-        </div>
+        </div-->
       </div>
       
       <div class="next-btn"><i class="fas fa-chevron-right" id="button"></i></div>
@@ -85,18 +147,38 @@
     <script src="main.js"></script>
     <div class="title"><strong>NEW PRODUCTS</strong><span><a href="#">MORE</a></span></div>
 
+    <?php
+      $sorted_by_date_products = sort_by_group($products, 'created_time', false);
+
+    ?>
+
+
+
 
     <div class="proprev-btn"><i class="fas fa-chevron-left"></i></div>
     <div class="wrapper-product">
       <div class="new-products">
+        <?php
+        for($i=0; $i < 10; $i++) {
+
+
+        ?>
+
         <div class='item-box'>
           <a href='mainpage/product/product-u1.html'><img src='img/Uniqlo/Men_UT.jpg' width='200px' height='250px' alt='T-shirt'></a>
           
-            <h3>MEN T-shirt </h3>
-            <p class='price'>399.000 VND</p>
+            <h3> <?=$sorted_by_date_products[$i]['name']?> </h3>
+            <p class='price'> <?=$sorted_by_date_products[$i]['price']?></p>
           
         </div>
-        <div class='item-box'>
+
+        <?php 
+          }
+        ?>
+
+
+
+        <!--div class='item-box'>
           <a href='mainpage/product/product-u3.html'><img src='img/Uniqlo/Child_UT.jpg' width='200px' height='250px' alt='T_shirt'></a>
           
             <h3>CHILD T-shirt</h3>
@@ -141,7 +223,7 @@
             <h3>Laptop APPLE ...</h3>
             <p class='price'>23.990.000 VND</p>
           </a>
-        </div>
+        </div-->
       </div>
       <div class="pronext-btn"><i class="fas fa-chevron-right"></i></div>
     </div>
@@ -153,57 +235,46 @@
     <div class="wrapper-ftstore">
       <div class="ftprev-btn"><i class="fas fa-chevron-left"></i></div>
       <div class="ftstores-container">
+
+        <?php
+          foreach ($featured_stores as $s) {
+        ?>
+
         <div class="ftstore-image">
           <a href='mainpage/store/store.html' style='text-decoration: none;'><img id="str-img"src='img/Uniqlo-logo(1).jpg' ></a>
+          <h3><?=$s['name']?></h3>
         </div>
-        <div class="ftstore-image">
-          <a href='mainpage/store/store2.html' style='text-decoration: none;'><img id ="str-img" src='img/MLB-OG(1).png' ></a>
-        </div>
-        <div class="ftstore-image">
-          <a href='mainpage/store/store3.html' style='text-decoration: none;'><img id="str-img" src='img/990__1511456189_555_McDonalds.png' ></a>
-        </div>
+
+        <?php
+          }
+        ?>
       </div>
       <div class="ftnext-btn"><i class="fas fa-chevron-right"></i></div>
     </div>
 
     <div class="title"><strong>FEATURED PRODUCTS</strong><span><a href="#">MORE</a></span></div>
+  
 
     <div class="proprev-btn"><i class="fas fa-chevron-left"></i></div>
     <div class="featured-products">
+
+      <?php
+        foreach ($featured_products as $p) {
+
+      ?>
+
       <div class='ftitem-box'>
         <a href='mainpage/product/product1.html'><img src='img/Uniqlo/Child_UT.jpg' alt='t-shirt'>
 
-          <h3>CHILD T-shirt</h3>
-          <p class='ftprice'>249.000 VND</p>
+          <h3><?=$p['name']?></h3>
+          <p class='ftprice'><?=$p['price']?></p>
         </a>
       </div>
-      <div class='ftitem-box'>
-        <a href='mainpage/product/product3.html'><img src='img/abi2.jpg' >
-        
-          <h3>Khô gà abi</h3>
-          <p class='ftprice'>35.000 VND</p>
-        </a>
-      </div>
-      <div class='ftitem-box'>
-        <a href='mainpage/product/product4.html'><img src='img/mac7.webp' >
-        
-          <h3>Laptop APPLE ...</h3>
-          <p class='ftprice'>23.990.000 VND</p>
-        </a>
-      </div>
-      <div class='ftitem-box'>
-        <a href='mainpage/product/product2.html'><img src='img/abi1.jpg' >
-          <h3>Cơm cháy siêu vị</h3>
-          <p class='ftprice'>26.000 VND</p>
-        </a>
-      </div>
-      <div class='ftitem-box'>
-        <a href='mainpage/product/product-u1.html'><img src='img/Uniqlo/Men_UT.jpg' width='200px' height='250px' alt='T-shirt'></a>
-        
-          <h3>MEN T-shirt </h3>
-          <p class='ftprice'>399.000 VND</p>
-        
-      </div>
+
+      <?php
+        }
+      ?>
+
     </div>
     <div class="pronext-btn"><i class="fas fa-chevron-right"></i></div>
   </main>
