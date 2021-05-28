35efad3f1c67af1991dfb1c6dbad0838a1966af2
Nguyen Ha Kieu Anh <s3818552@rmit.edu.vn>
2021-05-28 14:41:49 +0700

    add file

--- account/login/login.php
+++ account/login/login.php
@@ -1,17 +1,1 @@
-<?php
-  session_start();
-
-  if (isset($_POST['act'])) {
-    if (isset($_POST['password']) && $_POST['password'] == 'password') {
-      $_SESSION['loggedin'] = true;
-      header('location: sampleuser.html');
-    } else {
-      $error = 'Invalid username/password';
-    }
-  }
-
-  if (isset($error)) {
-    echo "<p>$error</p>";
-  }
-?>
 
