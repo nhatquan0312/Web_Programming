99bb85c5a549f440b7634e59f713993d33801280
Nguyen Ha Kieu Anh <s3818552@rmit.edu.vn>
2021-05-29 17:24:43 +0700

    login

--- account/login/login.js
+++ account/login/login.js
@@ -1,18 +1,24 @@
 function validate() {
     var user = document.getElementById("username").value;
-        if (user == "") { document.getElementById('user').innerHTML = "Required field"; 
-    
-    document.getElementById('username').classList.add("form-error");                    
-          return false;
-        }
-      else {
-    document.getElementById('username').classList.add("form-success"); 
-      }
-      
-    var pass =document.getElementById("passWORD").value;
-        if (pass == "") { document.getElementById('pass').innerHTML = "Required field";           
-          return false;
-        }
-    }
+      if (user.length == 0 ||  user.length <= 3) { document.getElementById('user').innerHTML = "Required field"; 
   
+  document.getElementById('username').classList.add("form-error");                    
+        return false;
+      }
+    else {
+  document.getElementById('username').classList.add("form-success"); 
+    }
+    var pass = document.getElementById("loginpass").value;
+      if (pass.length == 0 ||  pass.length <= 3) { document.getElementById('lpass').innerHTML = "Required field"; 
+  
+  document.getElementById('loginpass').classList.add("form-error");                    
+        return false;
+      }
+    else {
+  document.getElementById('lpass').classList.add("form-success"); 
+    return true;
+    }
+    
+  
+  }
   
