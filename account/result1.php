15852f514ffcb17d2600ffc5f900e13b495b658c
Nguyen Ha Kieu Anh <s3818552@rmit.edu.vn>
2021-06-01 01:59:37 +0700

    add file

--- account/result1.php
+++ account/result1.php
@@ -1,25 +1,10 @@
 <?php 
-$username = $_POST['username'];
-$password = $_POST['password'];
 
-if(emty($username)) {
-    $name_error ="Please insert your name";
-}
-if(emty($password)) {
-    $pass_error ="Please insert your password";
-}
+echo '<script type="text/javascript">',
+     'validate();',
+     '</script>'
+;
+
+include ('myaccount.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Username: <?php echo $username ?></h1>
-    <h2>Username: <?php echo $username ?></h2>
-</body>
-</html>
