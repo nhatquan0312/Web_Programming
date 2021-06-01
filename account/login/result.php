618be1da000394e1d344d871cae99e848a73a469
Nguyen Ha Kieu Anh <s3818552@rmit.edu.vn>
2021-06-01 22:35:12 +0700

    last edition

--- account/login/result.php
+++ account/login/result.php
@@ -14,12 +14,7 @@ function textClean($string)
 
 $user = "";
 $pass = "";
-if (isset($_POST['user'])) { 
-    $user = $_POST['user'];
-}
-if (isset($_POST['pass'])) { 
-    $pass = $_POST['pass'];
-}    
+  
 
 $useroptions = ['cost' => 10,]; 
 $pwoptions   = ['cost' => 10,]; 
@@ -29,22 +24,8 @@ $hasheduser  = file_get_contents("store_data.csv");
 $hashedpass  = file_get_contents("store_data.csv"); 
 
 
-if ((password_verify($user, $hasheduser)) && (password_verify($pass,$hashedpass))) {
 
-    include "pass-protected.php";
 
-} else { 
-    // if it was invalid it'll just display the form, if there was never a $_POST
-    // then it'll also display the form. that's why I set $user to "" instead of a $_POST
-    // this is the right place for comments, not inside html
-    ?>  
-    <form method="POST" action="index.php">
-    User <input type="text" name="user"></input><br/>
-    Pass <input type="password" name="pass"></input><br/>
-    <input type="submit" name="submit" value="Go"></input>
-    </form>
-    <?php 
-} 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($username)) {
         $user_error ="Please enter you username";
@@ -52,25 +33,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $success ="Success";
         $username = $username;
     }
-    
-    if(empty($password)){
-        $pass_error = "Please confirm password";     
-    } else {
-        $success ="Success";
-        $password = $password;
+
+  if (isset($_POST['submit'])) {
+    if ($password == 'password') {
+      $_SESSION['loggedin'] = true;
+      header('location: sampleuser.html');} 
+      else {
+      $pass_error = 'Invalid username/password';
     }
+  }
 
-    
-    //go to page
-    if(isset($_POST['submit'])) {
-        header('Location: /index.php'); // 'Location: /account/orderplacement.php'
-    }
-    include ('login.php');
-
-}
-?>
-
-
-
-
+      ?>
 
