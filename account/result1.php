eea685291a2a0d85d7705352549f4dd846765af1
Nguyen Ha Kieu Anh <s3818552@rmit.edu.vn>
2021-06-01 12:32:41 +0700

    store

--- account/result1.php
+++ account/result1.php
@@ -1,15 +1,5 @@
 <?php 
 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$zipcode = $_POST['zipcode'];
-$city= $_POST['city'];
-$address = $_POST['address'];
-$country = $_POST['country'];
-$username = $_POST['username'];
-$password = $_POST['password'];
-$repass = $_POST['repassword'];
-
 function textClean($string)
 {
  $string = trim($string);
@@ -17,10 +7,22 @@ function textClean($string)
  $string = htmlspecialchars($string);
  return $string;
 }
+
+$firstname = textClean($_POST['firstname']);
+$lastname = textClean($_POST['lastname']);
+$zipcode = textClean($_POST['zipcode']);
+$city= textClean($_POST['city']);
+$address = textClean($_POST['address']);
+$country = textClean($_POST['country']);
+$username = textClean($_POST['username']);
+$password = textClean($_POST['password']);
+$repass = textClean($_POST['repassword']);
+$error= "";
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($lastname)) {
-        $lname_error ="This field is required";
-    } if(textClean(strlen($lastname)) <3 ) {
+        $lname_error="This field is required";
+    } if(strlen($lastname) <3 ) {
         $lname_error ="Your last name needs to have a minimum length of 6";
     } else {
         $success ="Success";
@@ -28,18 +30,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     if(empty($firstname)) {
         $fname_error ="This field is required";
-    } if(textClean(strlen($firstname)) <3 ) {
+    } if(strlen($firstname) <3) {
         $fname_error ="Your first name needs to have a minimum length of 6";
     } else {
         $success ="Success";
         $firstname = $firstname;
     }
     if(empty($zipcode)) {
-        $zip_error ="This field is required";
-    } if(!is_nan(floatVal($zipcode))) {
-        $zip_error ="This field only contains digits";
+        $zcode_error ="This field is required";
+    } if(!is_nan($zipcode)) {
+        $zcode_error ="This field only contains digits";
     } if(strlen($zipcode) <3 || strlen($zipcode) >7 ) {
-        $zip_error ="Zipcode must contain 4 to 6 digits";
+        $zcode_error ="Zipcode must contain 4 to 6 digits";
     } else {
         $success ="Success";
         $zipcode = $zipcode;
@@ -66,25 +68,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $success ="Success";
         $country = $country;
     }
-    if(empty(trim($username))) {
+    if(empty($username)) {
         $user_error ="This field is required";
-    } if(!preg_match('/^(([a-zA-Z0-9][.]?){2,}|([a-zA-Z0-9]\.)+)([a-zA-Z0-9]|(?!\.))+?[a-zA-Z0-9]@(([a-zA-Z0-9]+\.)+[a-zA-Z]{2,5})$/', trim($username))) {
-        $user_error ="Username can only contain letters, numbers, and underscores";
+    } if(!preg_match('/^(([a-zA-Z0-9][.]?){2,}|([a-zA-Z0-9]\.)+)([a-zA-Z0-9]|(?!\.))+?[a-zA-Z0-9]@(([a-zA-Z0-9]+\.)+[a-zA-Z]{2,5})$/', $username)){
+        $user_error ="$email is not a valid email address";
     } else {
-        $success ="Success";
+        $success ="$username is a valid email address";
         $username = $username;
     }
-    if(empty(trim($password))){
+    if(empty($password)){
         $pass_error = "Please enter your password";     
     } if(strlen($password) <3 ) {
         $pass_error ="Your password needs to have a minimum length of 6";
-    } if(!preg_match('/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])$/', trim($password))) {
-        $pass_error ="Passwords must contain 8 to 20 characters, no space, with at least 1 lower case letter, at least 1 upper case letter, at least 1 digit, and at least 1 special character in the set !@#$%^&*";
+    } if(!preg_match('/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])$/', $password)) {
+        $pass_error ="Passwords must contain 8 to 20 characters, no space, with at least 1 lower case letter, 1 upper case letter, 1 digit, 1 special character in the set !@#$%^&*";
     } else {
         $success ="Success";
-        $password = trim($password);
+        $password = $password;
     }
-    if(empty(trim($repass))){
+    if(empty($repass)){
         $repass_error = "Please confirm password";     
     } if(strlen($password) <3 ) {
         $repass_error ="Your password needs to have a minimum length of 6";
@@ -92,11 +94,35 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $repass_error ="Passwords does not match";
     } else {
         $success ="Success";
-        $repass = trim($repass);
+        $repass = $repass;
     }
-    include("myaccount.php");
+    if($success !== '')
+    {
+     $file_open = fopen("store_data.csv", "a");
+     $no_rows = count(file("store_data.csv"));
+     if($no_rows > 1)
+     {
+      $no_rows = ($no_rows - 1) + 1;
+     }
+     $form_data = array(
+      'sr_no'  => $no_rows,
+      'firstname'  => $firstname,
+      'lastname'  => $lastname,
+      'zipcode'  => $zipcode,
+      'email'  => $username,
+      'city' => $city,
+      'address' => $address,
+      'email'  => $username,
+      'password'  => $password,
+      'confirm pass'  => $repass,
+     );
+     fputcsv($file_open, $form_data);
+     $label = '<label class="heading">Thank you for contacting us</label>';
+     $name = '';
+     $email = '';
+     $subject = '';
+     $message = '';
+    }
 }
-
-
-    
+   
 ?>
